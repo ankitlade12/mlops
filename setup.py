ff053fb1a10aa1ceae5cfea0fdb59c6717fae889
ankitlade12 <ankitlade12@gmail.com>
2021-11-02 12:02:51 -0500

    structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="MLOPS Package",
+    author="ankitlade12",
+    packages=find_packages(),
+    license="MIT",
+)
